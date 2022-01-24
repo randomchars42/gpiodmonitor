e1558bcfbf38f0ccb47c980fa3f0d6ff71522155
randomchars42 <randomchars42@users.noreply.github.com>
2022-01-24 16:21:19 +0100

    remove classifier for python 3.6 as it is no longer supported

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         # that you indicate you support Python 3. These classifiers are *not*
         # checked by 'pip install'. See instead 'python_requires' below.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -59,7 +58,7 @@ setup(
     setup_requires=[
         'docutils>=0.3',
         'wheel',
-        'setuptools_scm',
+        'setuptools',
     ],
     install_requires=[
     ],
